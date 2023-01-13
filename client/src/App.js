92c969831a84d4127b4397e27851db4da665f14c
mariyabaig <mariya.baig@nagarro.com>
2023-01-13 17:32:41 +0530

    UI refactored

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Login from './views/Login';
+import Register from './views/Register';
 
 function App() {
   return (
    <>
-    
+    <Login/>
+    {/* <Register/> */}
    </>
   );
 }
