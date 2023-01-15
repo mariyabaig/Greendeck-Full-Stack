f64414935627b294c4bb70426a006935a197e4d4
mariyabaig <mariya.baig@nagarro.com>
2023-01-15 20:11:28 +0530

    Heading added

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from '../components/Header'
+import Heading from '../components/Heading'
 
 const Home = () => {
   return (
     <div>
-      
+      <Header/>
+      <Heading/>
     </div>
   )
 }
