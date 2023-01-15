231d8125b949cfb6ef4cbc174534c1f119e89d75
mariyabaig <mariya.baig@nagarro.com>
2023-01-15 20:16:01 +0530

    added navigation

--- client/src/views/Register.js
+++ client/src/views/Register.js
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const Register = () => {
+  const navigate = useNavigate()
+  const goToLogin=()=>{
+navigate("/login")
+  }
+const register=()=>{
+const{name, email,password, cpassword} = user
+if(name && email && password && (password===cpassword)){
+  axios.post("http://localhost:9000/register",user).then(res=>console.log(res))
+
+}else{
+  alert("invalid")
+}
+
+  }
+
   const [user, setUser] = useState({
-    name: " ",
-    email: " ",
-    password: " ",
-    cpassword: " ",
+    name:" ",
+    email:" ",
+    password:" ",
+    cpassword:" ",
   });
 
   const handleChange = (e) => {
@@ -26,7 +42,7 @@ const Register = () => {
             <div>
               <div className="flex items-center justify-center space-x-4 mt-3">
                 <button className="flex items-center py-2 px-4 text-sm uppercase rounded bg-white hover:bg-gray-100 text-indigo-500 border border-transparent hover:border-transparent hover:text-gray-700 shadow-md hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-                  {/* <svg
+                  <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 16 16"
                     className="w-6 h-6 mr-3"
@@ -35,11 +51,11 @@ const Register = () => {
                       fill-rule="evenodd"
                       d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
                     ></path>
-                  </svg> */}
+                  </svg> 
                   Github
                 </button>
                 <button className="flex items-center py-2 px-4 text-sm uppercase rounded bg-white hover:bg-gray-100 text-indigo-500 border border-transparent hover:border-transparent hover:text-gray-700 shadow-md hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-                  {/* <svg
+                  <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="w-6 h-6 mr-3"
                     viewBox="0 0 48 48"
@@ -60,7 +76,7 @@ const Register = () => {
                       fill="#1565c0"
                       d="M43.611 20.083 43.595 20H24v8h11.303a12.04 12.04 0 0 1-4.087 5.571l.003-.002 6.19 5.238C36.971 39.205 44 34 44 24c0-1.341-.138-2.65-.389-3.917z"
                     />
-                  </svg> */}
+                  </svg> 
                   Google
                 </button>
               </div>
@@ -82,14 +98,14 @@ const Register = () => {
                   onChange={handleChange}
                 />
                 <div className="absolute left-0 inset-y-0 flex items-center">
-                  {/* <svg
+                  <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-7 w-7 ml-3 text-gray-400 p-1"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                   >
                     <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
-                  </svg> */}
+                  </svg> 
                 </div>
               </div>
 
@@ -104,7 +120,7 @@ const Register = () => {
                   onChange={handleChange}
                 />
                 <div className="absolute left-0 inset-y-0 flex items-center">
-                  {/* <svg
+                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-7 w-7 ml-3 text-gray-400 p-1"
                     viewBox="0 0 20 20"
@@ -112,7 +128,7 @@ const Register = () => {
                   >
                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                     <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                  </svg> */}
+                  </svg> 
                 </div>
               </div>
               <div className="relative mt-3">
@@ -126,14 +142,14 @@ const Register = () => {
                   onChange={handleChange}
                 />
                 <div className="absolute left-0 inset-y-0 flex items-center">
-                  {/* <svg
+                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-7 w-7 ml-3 text-gray-400 p-1"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                   >
                     <path d="M10 2a5 5 0 00-5 5v2a2 2 0 00-2 2v5a2 2 0 002 2h10a2 2 0 002-2v-5a2 2 0 00-2-2H7V7a3 3 0 015.905-.75 1 1 0 001.937-.5A5.002 5.002 0 0010 2z" />
-                  </svg> */}
+                  </svg> 
                 </div>
               </div>
 
@@ -148,23 +164,32 @@ const Register = () => {
                   onChange={handleChange}
                 />
                 <div className="absolute left-0 inset-y-0 flex items-center">
-                  {/* <svg
+                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-7 w-7 ml-3 text-gray-400 p-1"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                   >
                     <path d="M10 2a5 5 0 00-5 5v2a2 2 0 00-2 2v5a2 2 0 002 2h10a2 2 0 002-2v-5a2 2 0 00-2-2H7V7a3 3 0 015.905-.75 1 1 0 001.937-.5A5.002 5.002 0 0010 2z" />
-                  </svg> */}
+                  </svg> 
                 </div>
               </div>
 
               <div className="flex items-center justify-center mt-8">
                 <button
-                  onClick={Register}
+                  onClick={register}
                   className="text-white py-2 px-4 uppercase rounded bg-indigo-500 hover:bg-indigo-600 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
                 >
                   Create Account
+                </button>
+              </div>
+              Already have an account?
+              <div className="flex items-center justify-center mt-8">
+                <button
+                  onClick={goToLogin}
+                  className="text-white py-2 px-4 uppercase rounded bg-indigo-500 hover:bg-indigo-600 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
+                >
+                 Log in
                 </button>
               </div>
             </form>
