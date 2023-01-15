d2325c9ffbe5d30940f65b3583cd95f668119965
mariyabaig <mariya.baig@nagarro.com>
2023-01-15 20:14:43 +0530

    Implemented routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
-import './App.css';
-import Login from './views/Login';
-import Register from './views/Register';
+import "./App.css";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./views/Home";
 
 function App() {
+  const [user, setLoginUser] = useState({});
   return (
-   <>
-    <Login/>
-    {/* <Register/> */}
-   </>
+    <>
+     <BrowserRouter>
+        <Routes> 
+          <Route
+            exact
+            path="/"
+            element={user && user._id ? <Home /> : <Login />}
+            
+          /> 
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter> 
+    </>
   );
 }
 
