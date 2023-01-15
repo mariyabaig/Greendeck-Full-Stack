c758f6d84bd9322f13c5163233db3b1c1deadfec
mariyabaig <mariya.baig@nagarro.com>
2023-01-15 20:13:36 +0530

    Refactored dashboard

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import Header from '../components/Header'
-import Heading from '../components/Heading'
-import TopDrivers from '../components/TopDrivers'
+import React from "react";
+import Header from "../components/Header";
+import Heading from "../components/Heading";
+import TopDrivers from "../components/TopDrivers";
 
 const Home = () => {
   return (
-    <div>
-      <Header/>
-      <Heading/>
-      <TopDrivers/>
-    </div>
-  )
-}
+    <>
+      <Header />
+      <div className="flex flex-row h-screen">
+        <div className=" w-3/4">
+          <Heading />
+          <TopDrivers />
+        </div>
+        <div className="w-1/4 ">
+          <Charts />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
