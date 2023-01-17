df0e2cc36645631b3134917bc1f576934587d1bc
mariyabaig <mariya.baig@nagarro.com>
2023-01-18 01:58:19 +0530

    Chart added

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -4,9 +4,20 @@ import Heading from "../components/Heading";
 import TopDrivers from "../components/TopDrivers";
 import Charts from "../components/Chart";
 import Change from "../components/Change";
+import one from '../company_logo_name.svg';
+
 const Home = () => {
   return (
     <>
+   <div className="flex justify-center items-center"
+        style={{
+          padding: '5px',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <img className = "my-3" src={one} style={{width:'200px'}}/>
+      </div>
       <Header />
       <div className="flex flex-row h-screen">
         <div className="mx-5 w-3/5">
@@ -18,8 +29,9 @@ const Home = () => {
          
         </div>
         <div className="w-2/5 ">
-          {/* <Charts /> */}
-          <Change/>
+         <Charts /> 
+         <div className="my-16"> <Change/></div>
+         
         </div>
       </div>
     </>
